860a478366ba6519c92a847664b4bd2a30e47dd0
SuperXRanzo <okanskyavenger@gmail.com>
2024-06-04 18:44:33 +0700

    menualException

---
+++ manual Exception/manual Exception.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		cout << "defult Pengecualian dieksekusi" << endl;
+	}
+	return 0;
+}
